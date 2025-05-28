096140df161e1f8c73a98479621340c9acefcbd9
Ryan Patino <ryan.patino5@gmail.com>
2025-05-28 09:52:07 -0500

    finished shopping cart exercise

--- ShoppingCart/src/Main.java
+++ ShoppingCart/src/Main.java
@@ -9,7 +9,21 @@ public class Main {
         double productPrice = 4.99;
         short productQuantity = 78;
 
+        // Write code to calculate the total cost of the product based on the inventory.
+        // Print the value.
+        double totalCost = productCost * productQuantity;
+        System.out.println("Total Cost: " + totalCost);
 
+
+        // Write code to calculate the profit in dollars of the product.
+        // Print the value.
+        double profit = productPrice - productCost;
+        System.out.println("Profit in dollars: " + profit);
+
+        // Write code to calculate the total potential profit.
+        // Print the value.
+        double potentialProfit = (profit * productQuantity);
+        System.out.println("Potential total profit: " + potentialProfit);
 
     }
 }
