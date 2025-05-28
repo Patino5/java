f1ef89e20ad347d49930d14e669baa7f9c654e1a
Ryan Patino <ryan.patino5@gmail.com>
2025-05-28 15:25:30 -0500

    Part 2 complete "Book Library System"

--- exercise-objects-references/src/Book.java
+++ exercise-objects-references/src/Book.java
@@ -1,2 +1,23 @@
 public class Book {
+    private String title;
+    private  String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author, boolean isAvailable){
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public void isCheckedOut(){
+        System.out.println(title + " is available: " + isAvailable);
+    }
+
+    public void borrowBook(){
+        System.out.println("Borrowing " + title + "...");
+        isAvailable = false;
+    }
+
+
+
 }
--- exercise-objects-references/src/ObjectPratice.java
+++ exercise-objects-references/src/ObjectPratice.java
@@ -7,5 +7,18 @@ public class ObjectPratice {
         car1.displayInfo();
         car2.displayInfo();
 
+        // Part 2 Book Libray System
+        Book book1 = new Book("The Hobbit", "J.R.R. Tolkien", true);
+        Book book2 = new Book("Cloudy with a Chance of Meatballs", "Judi Barrett", true);
+
+        book1.isCheckedOut();
+        book2.isCheckedOut();
+
+        book1.borrowBook();
+        book2.borrowBook();
+
+        book1.isCheckedOut();
+        book2.isCheckedOut();
+
     }
 }
