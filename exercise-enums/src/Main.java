423c67ccb0bedc27d8a535bd744482cdc9eb27a9
Ryan Patino <ryan.patino5@gmail.com>
2025-05-28 12:08:33 -0500

    Part 2 complete

--- exercise-enums/src/Main.java
+++ exercise-enums/src/Main.java
@@ -7,11 +7,22 @@ public class Main {
         MEDIUM,
         LARGE
     }
+
+    enum SeatSection {
+        GENERAL,
+        PREMIUM,
+        VIP
+    }
     public static void main(String[] args) {
 
         CoffeeSize order = CoffeeSize.SMALL;
 
         System.out.println(" Selected coffee size: " + order);
+
+        System.out.println(SeatSection.GENERAL + " is assigned value: " + SeatSection.GENERAL.ordinal());
+        System.out.println(SeatSection.PREMIUM + " is assigned value: " + SeatSection.PREMIUM.ordinal());
+        System.out.println(SeatSection.VIP + " is assigned value: " + SeatSection.VIP.ordinal());
+
 
     }
 }
