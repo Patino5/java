8fdc665dbc40fcc5c1f54eb378d7446c52ff479f
Ryan Patino <ryan.patino5@gmail.com>
2025-05-29 14:27:29 -0500

    Exercise Console IO completed

--- exercise_console_io/src/Main.java
+++ exercise_console_io/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Welcome to the order form!");
         System.out.print("Enter your name: ");
         userName = io.nextLine();
-        System.out.printf("Hello %s, let's get started with your order.", userName);
+        System.out.printf("Hello %s, let's get started with your order.%n", userName);
 
         System.out.print("What product would you like to purchase? ");
         productName = io.nextLine();
@@ -23,8 +23,22 @@ public class Main {
         System.out.print("What is the unit price? ");
         unitPrice = Double.parseDouble(io.nextLine());
 
+        // Calculate totals
+        double subTotal = quantity * unitPrice;
+        double taxRate = 8.25;
+        double taxAmount = subTotal * (taxRate / 100);
+        double grandTotal = subTotal + taxAmount;
 
-
+        // formated receipt
+        System.out.println("Order Summary");
+        System.out.println("----------------------");
+        System.out.printf("Item: %14s%nQuantity: %7s%nUnit Price:     $%.2f%n", productName, quantity, unitPrice);
+        System.out.println("----------------------");
+        System.out.printf("Subtotal:       $%.2f%n", subTotal);
+        System.out.printf("Tax (%.2f%%):    $%.2f%n", taxRate, taxAmount);
+        System.out.printf("Grand Total:    $%.2f%n", grandTotal);
+        System.out.println("----------------------");
+        System.out.println("Thank you for your order, " + userName);
 
     }
 }
