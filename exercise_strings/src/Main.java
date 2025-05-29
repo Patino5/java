2ca777d3b1aacf4aa0f0cadf9385894ad48f83f3
Ryan Patino <ryan.patino5@gmail.com>
2025-05-29 11:43:25 -0500

    Part 1: Basic String Operations completed

--- exercise_strings/src/Main.java
+++ exercise_strings/src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Harry";
+        String lastName = "Potter";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full name: " + fullName);
+        System.out.println("Length: " + fullName.length());
+
+        char firstLetter = fullName.charAt(0);
+        System.out.println("First character: " + firstLetter);
+        System.out.println("Index of 'r': " + fullName.indexOf('r'));
+
 
     }
 }
