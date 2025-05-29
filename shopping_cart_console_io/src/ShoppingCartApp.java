ac01a86abcf7c3fd254183ffa807d41be02b37fd
Ryan Patino <ryan.patino5@gmail.com>
2025-05-29 14:31:41 -0500

    initial commit for shopping_cart_console_io

---
+++ shopping_cart_console_io/src/ShoppingCartApp.java
@@ -0,0 +1,35 @@
+public class ShoppingCartApp {
+    public static void main(String[] args) {
+
+        // Product Info
+        byte productId = 1;
+        byte productCategory = 2;
+        double productCost = 2.56;
+        double productPrice = 4.99;
+        short productQuantity = 78;
+
+        // Write code to calculate the total cost of the product based on the inventory.
+        // Print the value.
+        double totalCost = productCost * productQuantity;
+        System.out.println("Total Cost: " + totalCost);
+
+
+        // Write code to calculate the profit in dollars of the product.
+        // Print the value.
+        double profit = productPrice - productCost;
+        System.out.println("Profit in dollars: " + profit);
+
+        // Write code to calculate the total potential profit.
+        // Print the value.
+        double potentialProfit = (profit * productQuantity);
+        System.out.println("Potential total profit: " + potentialProfit);
+
+        // Exercise for Shopping Cart Strings
+        String businessName = "Visa";
+        String contactInformation = "visa.com";
+        String productDescription = "A global payments technology company";
+
+        System.out.printf("Business name: %s%nContact Information: %s%nProduct Description: %s%n", businessName, contactInformation, productDescription);
+
+    }
+}
