32bb71a3ab624a532fb56379f748ccf5b4db6c45
Ryan Patino <ryan.patino5@gmail.com>
2025-05-30 14:58:52 -0500

    multiplication table complete

--- exercise_loops/src/Main.java
+++ exercise_loops/src/Main.java
@@ -4,32 +4,45 @@ public class Main {
     public static void main(String[] args) {
         // Basic loops Practice
         // count up (for Loop)
-        for (int i = 1; i < 101; i++){
-            System.out.println(i);
-        }
-
-        // Countdown Timer (while Loop)
+//        for (int i = 1; i < 101; i++){
+//            System.out.println(i);
+//        }
+//
+//        // Countdown Timer (while Loop)
         Scanner io = new Scanner(System.in);
-        System.out.print("Enter a starting number: ");
-        int startNum = Integer.parseInt(io.nextLine());
-        while (startNum > 0){
-            System.out.println(startNum);
-            startNum--;
-            if (startNum == 0){
-                System.out.println(startNum);
-                System.out.println("Blast off!");
-            }
+//        System.out.print("Enter a starting number: ");
+//        int startNum = Integer.parseInt(io.nextLine());
+//        while (startNum > 0){
+//            System.out.println(startNum);
+//            startNum--;
+//            if (startNum == 0){
+//                System.out.println(startNum);
+//                System.out.println("Blast off!");
+//            }
+//        }
+//
+//        // guess the number
+//        // generate random number between 0.0 and 1 exclusive Math.random()
+//        // random number between 0 and 100
+//        int randomNum = (int)(Math.random() * 51);
+//        int guess;
+//        do {
+//            System.out.print("Guess a number between 1 and 50: ");
+//            guess = Integer.parseInt(io.nextLine());
+//        } while (guess != randomNum);
+
+        // Intermediate Loops Challenges
+
+        // Multiplication Table (For Loop)
+        // ask for number
+        System.out.print("Enter a number: ");
+        int number = Integer.parseInt(io.nextLine());
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i + " * " + number + " = "+ (number * i));
         }
 
-        // guess the number
-        // generate random number between 0.0 and 1 exclusive Math.random()
-        // random number between 0 and 100
-        int randomNum = (int)(Math.random() * 51);
-        int guess;
-        do {
-            System.out.print("Guess a number between 1 and 50: ");
-            guess = Integer.parseInt(io.nextLine());
-        } while (guess != randomNum);
+        //
 
     }
 }
