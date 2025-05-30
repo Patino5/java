110e346ca95bb7c7cf0a53c1bea4d9402a07b52c
Ryan Patino <ryan.patino5@gmail.com>
2025-05-30 14:26:51 -0500

    count up completed

--- exercise_loops/src/Main.java
+++ exercise_loops/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Basic loops Practice
+        // count up
+        for (int i = 1; i < 101; i++){
+            System.out.println(i);
+        }
+
 
     }
 }
