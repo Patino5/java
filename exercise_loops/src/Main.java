7a2684b6e1688cf643b902c9350f11b89dda023c
Ryan Patino <ryan.patino5@gmail.com>
2025-05-30 15:21:46 -0500

    find the first vowel (for loop) complete

--- exercise_loops/src/Main.java
+++ exercise_loops/src/Main.java
@@ -35,22 +35,34 @@ public class Main {
 
         // Multiplication Table (For Loop)
         // ask for number
-        System.out.print("Enter a number: ");
-        int number = Integer.parseInt(io.nextLine());
+//        System.out.print("Enter a number: ");
+//        int number = Integer.parseInt(io.nextLine());
+//
+//        for (int i = 1; i <= 10; i++){
+//            System.out.println(i + " * " + number + " = "+ (number * i));
+//        }
+//
+//        // Password Validator (While Loop)
+//        // Ask user to enter a password
+//        // keep asking until they enter the correct password "letmein"
+//        String password = "";
+//
+//        while (!password.equals("letmein")){
+//            System.out.print("Enter the password: ");
+//            password = io.next();
+//        }
 
-        for (int i = 1; i <= 10; i++){
-            System.out.println(i + " * " + number + " = "+ (number * i));
+        // Find the first vowel (for loop)
+        System.out.print("Enter a word: ");
+        String word = io.nextLine();
+        String vowels = "aeiou";
+
+        for (int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if (vowels.indexOf(c) != -1){
+                System.out.println("First vowel '" + c + "' found at position: " + i );
+                break;
+            }
         }
-
-        // Password Validator (While Loop)
-        // Ask user to enter a password
-        // keep asking until they enter the correct password "letmein"
-        String password = "";
-
-        while (!password.equals("letmein")){
-            System.out.print("Enter the password: ");
-            password = io.next();
-        }
-
     }
 }
