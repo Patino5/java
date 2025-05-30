0d290146b6d3a7e5a0fbd7d69b0288a4dabb7c71
Ryan Patino <ryan.patino5@gmail.com>
2025-05-30 14:33:45 -0500

    Countdown timer completed

--- exercise_loops/src/Main.java
+++ exercise_loops/src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Basic loops Practice
-        // count up
+        // count up (for Loop)
         for (int i = 1; i < 101; i++){
             System.out.println(i);
         }
 
+        // Countdown Timer (while Loop)
+        Scanner io = new Scanner(System.in);
+        System.out.print("Enter a starting number: ");
+        int startNum = Integer.parseInt(io.nextLine());
+        while (startNum > 0){
+            System.out.println(startNum);
+            startNum--;
+            if (startNum == 0){
+                System.out.println(startNum);
+                System.out.println("Blast off!");
+            }
+        }
 
     }
 }
