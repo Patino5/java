9a245cfe8ec078d52305fa5acebb2909061fb5ec
Ryan Patino <ryan.patino5@gmail.com>
2025-06-02 12:38:02 -0500

    Task 2: sum of two numbers complete

--- exercise_methods/src/Main.java
+++ exercise_methods/src/Main.java
@@ -2,9 +2,20 @@ public class Main {
     public static void main(String[] args) {
         // Task 1: Define and Call a Simple Method
         printWelcomeMessage();
+
+        // Task 2: Calculate the Sum of Two Numbers
+        System.out.println(sum(3, 6));
+        System.out.println(sum(500, 3));
+        System.out.println(sum(-34, -55));
+
     }
     // printWelcomeMessage
     public static void printWelcomeMessage() {
         System.out.println("Welcome to the Java Methods Exercise!");
     }
+    // sum
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
 }
