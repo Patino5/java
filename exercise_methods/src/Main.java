2c18303c7c71e346e63429500a8336a43cf89011
Ryan Patino <ryan.patino5@gmail.com>
2025-06-02 12:32:46 -0500

    Task 1 of exercise methods complete

---
+++ exercise_methods/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Task 1: Define and Call a Simple Method
+        printWelcomeMessage();
+    }
+    // printWelcomeMessage
+    public static void printWelcomeMessage() {
+        System.out.println("Welcome to the Java Methods Exercise!");
+    }
+}
