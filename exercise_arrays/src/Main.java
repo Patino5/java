f589ba03fdc4a2bd08ac6783930e794ded4286f7
Ryan Patino <ryan.patino5@gmail.com>
2025-06-02 00:48:30 -0500

    exercise arrays part 2 complete

--- exercise_arrays/src/Main.java
+++ exercise_arrays/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Part 1: Basic Array Operations
@@ -16,6 +18,32 @@ public class Main {
         System.out.println(cities[2]);
         // 3. Find the Length of an Array
         System.out.println("Number of cities in the array: " + cities.length);
+
+        // Part 2: Iterating Over Arrays
+        // 4. Print Array elements using a loop
+        for (String city : cities) {
+            System.out.println(city);
+        }
+        // 5. Reverse an array
+        for (int i = cities.length -1; i >= 0; i--){
+            System.out.println(cities[i]);
+        }
+        // 6. Find a specific element
+        // ask for a city from user
+        Scanner io = new Scanner(System.in);
+        System.out.print("Enter a city: ");
+        String userInput = io.nextLine();
+        boolean foundCity = false;
+        for (String city : cities){
+            if (city.equalsIgnoreCase(userInput)) {
+                System.out.println("City found!");
+                foundCity = true;
+                break;
+            }
+        }
+        if (!foundCity) {
+            System.out.println("City not found");
+        }
 
 
     }
