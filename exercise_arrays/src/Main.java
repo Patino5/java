80023161bd0d0a5b9db77e65c4e898c7f5ad77e0
Ryan Patino <ryan.patino5@gmail.com>
2025-06-02 00:32:07 -0500

    exercise arrays pt1 solution

--- exercise_arrays/src/Main.java
+++ exercise_arrays/src/Main.java
@@ -2,8 +2,20 @@ public class Main {
     public static void main(String[] args) {
         // Part 1: Basic Array Operations
         // 1. Declare an array
+        // create an array of 5 cities and assign values directly
+        String[] cities = {"Austin", "Round Rock", "Cedar Park", "Leander", "Georgetown"};
+        System.out.println(cities[0]);
+        System.out.println(cities[1]);
+        System.out.println(cities[2]);
+        System.out.println(cities[3]);
+        System.out.println(cities[4]);
+
         // 2. Access & Modify Elements
+        // update the 3rd city in the array to a different name
+        cities[2] = "San Antonio";
+        System.out.println(cities[2]);
         // 3. Find the Length of an Array
+        System.out.println("Number of cities in the array: " + cities.length);
 
 
     }
