5bc0e7b995c66e5283b34e5a7187c5d62d49374e
Ryan Patino <ryan.patino5@gmail.com>
2025-06-02 00:23:05 -0500

    exercise arrays part 1 start

---
+++ exercise_arrays/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Part 1: Basic Array Operations
+        // 1. Declare an array
+        // 2. Access & Modify Elements
+        // 3. Find the Length of an Array
+
+
+    }
+}
