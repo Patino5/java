e7e27285bebcbb9994c0f37561c533294d54d552
Ryan Patino <ryan.patino5@gmail.com>
2025-06-03 15:10:36 -0500

    made Scanner a class field and added menuoption method to better use try catch blocks

--- shopping_cart_exceptions/src/ShoppingCartApp.java
+++ shopping_cart_exceptions/src/ShoppingCartApp.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ShoppingCartApp {
+    private static Scanner io = new Scanner(System.in);
     public static void main(String[] args) {
         // Product Info
         byte productId = 1;
@@ -12,17 +13,14 @@ public class ShoppingCartApp {
         Scanner io = new Scanner(System.in);
 
         // Write code to calculate the total cost of the product based on the inventory.
-        // Print the value.
         double totalCost = productCost * productQuantity;
         System.out.println("Total Cost: " + totalCost);
 
         // Write code to calculate the profit in dollars of the product.
-        // Print the value.
         double profit = productPrice - productCost;
         System.out.println("Profit in dollars: " + profit);
 
         // Write code to calculate the total potential profit.
-        // Print the value.
         double potentialProfit = (profit * productQuantity);
         System.out.println("Potential total profit: " + potentialProfit);
 
@@ -34,22 +32,16 @@ public class ShoppingCartApp {
         System.out.printf("Business name: %s%nContact Information: %s%nProduct Description: %s%n", businessName, contactInformation, productDescription);
 
         // console io additions
-        String taxExempt;
-        String shipping;
-        int orderQuantity;
-        String promoCode;
-
-        // collect data
-        taxExempt = promptUserForString("Are you tax-exempt? (y/n) ");
-        shipping = promptUserForString("Shipping? (standard/overnight/twoday");
-        orderQuantity = promptUserForInt("Order quantity: ");
-        promoCode = promptUserForString("Promo code for free shipping: ");
+        String taxExempt = promptUserForString("Are you tax-exempt? (y/n) ");
+        String shipping = promptUserForString("Shipping? (standard/overnight/twoday");
+        int orderQuantity = promptUserForInt("Order quantity: ");
+        String promoCode = promptUserForString("Promo code for free shipping: ");
 
         // Loops additions - isConfirmed boolean as well
 
         while (!isConfirmed) {
-            System.out.println("Confirm Order y/n: ");
-            isConfirmed = "y".equals(io.nextLine());
+            System.out.print("Confirm Order y/n: ");
+            isConfirmed = "y".equalsIgnoreCase(io.nextLine().trim());
         }
 
         // print details from data collection
@@ -63,20 +55,18 @@ public class ShoppingCartApp {
         String[] shippingAddresses = {"123 Main St", "456 Main St", "789 Main St"};
         String[] productSizes = {"small", "medium", "large"};
 
-        int addressIndex = 0;
-        int sizeIndex = 0;
+
         // Shipping address menu
-        displayChoices(shippingAddresses);
-        addressIndex = promptUserForInt("Shipping Address: ");
+        int addressIndex = promptMenuOption("Shipping Address: ", shippingAddresses);
 
         // productSize menu
-        displayChoices(productSizes);
-        sizeIndex = promptUserForInt("What size: ");
+
+        int sizeIndex = promptMenuOption("What size: ", productSizes);
 
         //Print details
         System.out.println("\nShipping and Size Details");
-        System.out.println("Address: " + shippingAddresses[addressIndex -1]);
-        System.out.println("Product Size: " + productSizes[sizeIndex - 1]);
+        System.out.println("Address: " + shippingAddresses[addressIndex]);
+        System.out.println("Product Size: " + productSizes[sizeIndex]);
 
     }
     // displayChoices - parameter array - displays a list of choices from an array
@@ -87,15 +77,37 @@ public class ShoppingCartApp {
     }
     // promptUserForInt - parameter is a prompt = returns an Integer
     public static int promptUserForInt(String prompt) {
-        java.util.Scanner io = new java.util.Scanner(System.in);
-        System.out.print(prompt);
-        return Integer.parseInt(io.nextLine());
+        int value;
+        while (true){
+            try {
+                System.out.print(prompt);
+                value = Integer.parseInt(io.nextLine());
+                return value;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+            }
+        }
     }
     // promptUserForString - parameter prompt for string - return String input
     public static String promptUserForString(String prompt) {
-        java.util.Scanner io = new java.util.Scanner(System.in);
         System.out.print(prompt);
         return io.nextLine();
     }
-
+    // prompt menu choices selection based on menu choices available
+    public static int promptMenuOption(String prompt, String[] options) {
+        int choice;
+        while (true){
+            displayChoices(options);
+            try {
+                choice = promptUserForInt(prompt);
+                if (choice >= 1 && choice <= options.length){
+                    return choice -1;
+                } else {
+                    System.out.println("Please enter a valid option.");
+                }
+            } catch (Exception e){
+                System.out.println("Exception message: " + e.getMessage());
+            }
+        }
+    }
 }
