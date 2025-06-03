872548a96d9f723c98908d0e11d597a0dc01b3ac
Ryan Patino <ryan.patino5@gmail.com>
2025-06-03 12:16:23 -0500

    try catch with loop for age complete

--- exercise_exception_handling/src/UserRegistration.java
+++ exercise_exception_handling/src/UserRegistration.java
@@ -3,20 +3,35 @@ import java.util.Scanner;
 public class UserRegistration {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner io = new Scanner(System.in);
 
-        System.out.print("Enter your age: ");
-        int age = Integer.parseInt(scanner.nextLine());
+        int age = getAge("Enter your age: ", io);
 
         System.out.print("Enter your email: ");
-        String email = scanner.nextLine();
+        String email = "k";
 
         System.out.print("Enter your 4-digit PIN: ");
-        int pin = Integer.parseInt(scanner.nextLine());
+        int pin = Integer.parseInt(io.nextLine());
 
         System.out.println("\nRegistration Successful!");
         System.out.println("Age: " + age);
         System.out.println("Email: " + email);
         System.out.println("PIN: " + pin);
+    }
+
+    public static int getAge(String prompt, Scanner io){
+        while (true){
+            System.out.print(prompt);
+            try {
+                int age = Integer.parseInt(io.nextLine());
+                if (age > 0 && age < 120) {
+                    return age;
+                } else {
+                    System.out.println("That is not within the age range, try again?");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("That is not a number");
+            }
+        }
     }
 }
