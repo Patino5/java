afa98e5fc123fdb3acc929242d8a861f69244847
Patino5 <ryan.patino5@gmail.com>
2025-06-04 12:14:03 -0500

    Core Feature 1. Display Main Menu

--- Storage_Lockers/src/Main.java
+++ Storage_Lockers/src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner io = new Scanner(System.in);
+
     public static void main(String[] args) {
+        boolean keepRunning = true;
+        String fullMenu = """
+                
+                What would you like to do next?
+                 1. Rent a Locker
+                 2. Access a Locker
+                 3. Release a Locker
+                 ---
+                 Any other key to exit.""";
+
+
+        while (keepRunning) {
+            print(fullMenu);
+            String menuChoice = io.nextLine();
+            switch (menuChoice) {
+                case "1" : {
+                    // more
+                    System.out.println("Rent Locker");
+                }
+                case "2" : {
+                    //more
+                    System.out.println("Access Locker");
+                }
+                case "3" : {
+                    // more
+                    System.out.println("Release Locker");
+                }
+                default : {
+                    System.out.println("Exit");
+                    keepRunning = false;
+                }
+            }
+        }
+
+
 
     }
+    public static void print(String message){
+        System.out.println(message);
+    }
+
 }
