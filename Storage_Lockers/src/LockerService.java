7813cf60f199dde06ecac9da57f2456028230d04
Patino5 <ryan.patino5@gmail.com>
2025-06-05 15:29:59 -0500

    started locker numbering at i + 1, revised menu display

--- Storage_Lockers/src/LockerService.java
+++ Storage_Lockers/src/LockerService.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 
 public class LockerService {
-    private static Locker[] lockers ;
+    private final Locker[] lockers ;
 
     public LockerService(int numberOfLockers){
         lockers = new Locker[numberOfLockers];
 
         for (int i = 0; i < lockers.length; i++){
-            lockers[i] = new Locker(i);
+            lockers[i] = new Locker(i + 1);
         }
     }
 
@@ -18,45 +18,44 @@ public class LockerService {
     public Result rentLocker() {
         for (int i = 0; i < lockers.length; i++) {
             if (lockers[i].getPin() == null) { // first empty locker
-                String randomPin = generatePin();
-                lockers[i].setPinNumber(randomPin);
-                return new Result(true, ("Locker Rented: Locker #" + lockers[i].getNumber() + "\nPIN for entry: " + lockers[i].getPin()));
+                lockers[i].setPinNumber(generatePin());
+                return new Result(true, ("Thank You!\nLocker Rented: Locker #" + lockers[i].getNumber() + "\nPIN for entry: " + lockers[i].getPin()));
             }
         }
         return new Result(false, "All lockers are full. Try again later");
     }
 
     public Result accessLocker(int lockerNum, String pin) {
-        Result lockerCheck  = validLocker(lockerNum);
+        Result lockerCheck  = activeLocker(lockerNum);
         if (!lockerCheck.getSuccess()) return lockerCheck;
 
-        Result pinCheck = correctPin(lockerNum, pin);
+        Result pinCheck = verifyPin(lockerNum, pin);
         if (!pinCheck.getSuccess()) return pinCheck;
 
         return new Result(true, "Success! Lock it up when you're done.");
     }
 
     public Result releaseLocker(int lockerNum, String pin, String confirm){
-        Result lockerCheck = validLocker(lockerNum);
+        Result lockerCheck = activeLocker(lockerNum);
         if (!lockerCheck.getSuccess()) return lockerCheck;
 
-        Result pinCheck = correctPin(lockerNum, pin);
+        Result pinCheck = verifyPin(lockerNum, pin);
         if (!pinCheck.getSuccess()) return pinCheck;
 
-        if(!confirm.equals("y")) return new Result(false, "Locker not released");
+        if(!confirm.equalsIgnoreCase("y")) return new Result(false, "Locker not released");
 
         lockers[lockerNum].resetPin();
         return new Result(true, "Locker #" + (lockerNum + 1) + " made available and PIN reset.");
     }
 
-    public Result validLocker(int lockerNum) {
+    public Result activeLocker(int lockerNum) {
         if (lockers[lockerNum].getPin() != null){
             return new Result(true ,"Current active locker.");
         }
         return new Result(false, "That locker is not rented. Try Again.");
     }
 
-    public Result correctPin(int lockerNum, String pin) {
+    public Result verifyPin(int lockerNum, String pin) {
         if (pin.equals(lockers[lockerNum].getPin())){
             return new Result(true, "Successful PIN Entry");
         }
@@ -70,21 +69,22 @@ public class LockerService {
     }
 
     public String getMenu(boolean areLockersFull){
-        return (areLockersFull) ? """
+        return (areLockersFull) ?
+                """
                 
                 What would you like to do next?
                 1. Lockers Currently Full
                 2. Access a Locker
                 3. Release a Locker
-                ---
-                Any other key to exit.""" : """
+                ---"""
+                :
+                """
                 
                 What would you like to do next?
                 1. Rent a Locker
                 2. Access a Locker
                 3. Release a Locker
-                ---
-                Any other key to exit.""";
+                ---""";
 
     }
 
