cbabc5d0593e2b1bd1af14cb63441278d9e3a1fd
Patino5 <ryan.patino5@gmail.com>
2025-06-09 12:07:51 -0500

    Added the fields, constructor, and methods for the LockerManager

--- exercise-storage-lockers/src/LockerManager.java
+++ exercise-storage-lockers/src/LockerManager.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class LockerManager {
+    private ArrayList<Locker> lockers;
+
+    public LockerManager(ArrayList<Locker> lockers) {
+        this.lockers = lockers;
+    }
+
+    public void addLocker(String lockerId){
+        lockers.add(new Locker(lockerId));
+    }
+
+    public String getLocker(String lockerId) {
+        for(Locker locker : lockers){
+            locker.getLockerId().equals(lockerId);
+            return String.format(locker.toString());
+        }
+        return String.format("%s Locker not found", lockerId);
+    }
+
+    public void removeLocker(String lockerId) {
+        for (Locker locker : lockers){
+            if (locker.getLockerId().equals(lockerId)){
+                lockers.remove(locker);
+                break;
+            }
+        }
+    }
+
+    public void displayAllLockers() {
+        for (int i = 0; i < lockers.size(); i++) {
+            System.out.println(lockers.get(i).toString());
+        }
+    }
+
 }
