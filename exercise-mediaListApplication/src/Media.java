af4a6e13b0361c3ae2ad44d4d2612f162708350a
Patino5 <ryan.patino5@gmail.com>
2025-06-10 14:42:47 -0500

    media abstract class added with initial commit

---
+++ exercise-mediaListApplication/src/Media.java
@@ -0,0 +1,15 @@
+public abstract class Media {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    abstract void play();
+
+    abstract String getDescription();
+}
