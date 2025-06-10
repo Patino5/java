73af3ffcc54869b7f0ca6fa19c0846dbe9c692dc
Patino5 <ryan.patino5@gmail.com>
2025-06-10 14:50:19 -0500

    Audio class added with getters and setters and override methods

---
+++ exercise-mediaListApplication/src/Audio.java
@@ -0,0 +1,30 @@
+public class Audio extends Media{
+    private int duration;
+    private String artist; // name of artist or performer
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    String getDescription() {
+        return "";
+    }
+
+    @Override
+    void play() {
+
+    }
+}
