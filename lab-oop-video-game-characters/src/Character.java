02cf0c1f17b41ff0dc9ef90ac6080cfc1c71d4dc
Patino5 <ryan.patino5@gmail.com>
2025-06-11 15:53:20 -0500

    Character class made

---
+++ lab-oop-video-game-characters/src/Character.java
@@ -0,0 +1,15 @@
+public class Character {
+    protected String name;
+    protected int health;
+    protected int attackPower;
+
+    public Character(String name, int health, int attackPower){
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public void attack(){
+        System.out.println("Attack!!!");
+    }
+}
