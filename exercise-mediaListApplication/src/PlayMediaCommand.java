6b4e06f0e5a4d31a3dbbc7910296c46b5b7087f2
Patino5 <ryan.patino5@gmail.com>
2025-06-11 13:31:22 -0500

    added check for  empty list first

--- exercise-mediaListApplication/src/PlayMediaCommand.java
+++ exercise-mediaListApplication/src/PlayMediaCommand.java
@@ -2,13 +2,16 @@ import java.util.ArrayList;
 
 public class PlayMediaCommand {
     public static void execute(MediaService service) {
-        // shows all media
-        TerminalUtils.print(service.getAllMedia());
-        // need to select what media from the list
-        // ask user for media name
-        Media playMedia = service.findMediaByName(TerminalUtils.getString("Enter media name to play: "));
-        //play selected media
-        playMedia.play();
-
+       if (service.isEmpty()) {
+           TerminalUtils.print(service.getAllMedia());
+       } else{
+            TerminalUtils.print(service.getAllMedia());
+            // need to select what media from the list
+            // ask user for media name
+            Media playMedia = service.findMediaByName(TerminalUtils.getString("Enter media name to play: "));
+            //play selected media
+            playMedia.play();
+        }
+       TerminalUtils.pressEnterToContinue();
     }
 }
