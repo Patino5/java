719f810b0f3451cfe2b25f843206ac70e1a20df2
Patino5 <ryan.patino5@gmail.com>
2025-06-10 23:29:43 -0500

    added if-else-if chain for menu selection in while loop

--- exercise-mediaListApplication/src/Main.java
+++ exercise-mediaListApplication/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        MediaService service = new MediaService();
 
+        while (true) {
+            TerminalUtils.displayMenu();
+            String choice = TerminalUtils.getMenuChoice();
+
+            if (choice.equals("1")) {
+                //add media
+
+            } else if (choice.equals("2")) {
+                // remove media
+
+            } else if (choice.equals("3")) {
+                // play media
+
+            } else if (choice.equals("4")) {
+               // list all media
+
+            } else {
+                // option 5 quit
+                break;
+            }
+
+        }
     }
 }
