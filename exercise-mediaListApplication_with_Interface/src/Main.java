69e3f1f51e86890a7dbe4024816b2ab9821750f0
Patino5 <ryan.patino5@gmail.com>
2025-06-12 14:37:36 -0500

    Added playable and describable interfaces

--- exercise-mediaListApplication_with_Interface/src/Main.java
+++ exercise-mediaListApplication_with_Interface/src/Main.java
@@ -2,27 +2,33 @@ public class Main {
     public static void main(String[] args) {
         MediaService service = new MediaService();
 
+        label:
         while (true) {
             TerminalUtils.displayMenu();
             String choice = TerminalUtils.getMenuChoice();
 
-            if (choice.equals("1")) {
-                //add media
-                AddMediaCommand.execute(service);
-            } else if (choice.equals("2")) {
-                // remove media
-                RemoveMediaCommand.execute(service);
-            } else if (choice.equals("3")) {
-                // play media
-                PlayMediaCommand.execute(service);
-            } else if (choice.equals("4")) {
-               // list all media
-                ListAllMediaCommand.execute(service);
+            switch (choice) {
+                case "1":
+                    //add media
+                    AddMediaCommand.execute(service);
+                    break;
+                case "2":
+                    // remove media
+                    RemoveMediaCommand.execute(service);
+                    break;
+                case "3":
+                    // play media
+                    PlayMediaCommand.execute(service);
+                    break;
+                case "4":
+                    // list all media
+                    ListAllMediaCommand.execute(service);
 
-            } else {
-                // option 5 quit
-                TerminalUtils.print("Thank you for using Media Libray");
-                break;
+                    break;
+                default:
+                    // option 5 quit
+                    TerminalUtils.print("Thank you for using Media Libray");
+                    break label;
             }
 
         }
