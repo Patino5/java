6dce1df3b92d53c8518f8a6cd127190916b5fc4b
Patino5 <ryan.patino5@gmail.com>
2025-06-13 13:00:55 -0500

    part 4

--- exercise-list/src/Main.java
+++ exercise-list/src/Main.java
@@ -13,5 +13,8 @@ public class Main {
         // part 3. print third student in list.
         System.out.println(students.get(2));
 
+        // part 4. Remove Element - second from list
+        students.remove(1);
+        System.out.println(students);
     }
 }
