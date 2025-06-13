8ceb4800a7638c0ba5dbb0305d0137f88fc812dd
Patino5 <ryan.patino5@gmail.com>
2025-06-13 12:57:33 -0500

    part 2

--- exercise-list/src/Main.java
+++ exercise-list/src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<String> students = new ArrayList<>();
 
+        students.add("Ryan");
+        students.add("Bob");
+        students.add("Billy");
+        students.add("Jay");
+        students.add("Ray");
+
     }
 }
