a48db1214630d1a3e9c3227de00911f2f999e1a8
Patino5 <ryan.patino5@gmail.com>
2025-06-13 15:08:02 -0500

    exercise Hash-Based Collections part 1

---
+++ exercise-hash-based-collections/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> monthMap = new HashMap<>();
+        monthMap.put(1, "January");
+        monthMap.put(2, "February");
+        monthMap.put(3, "March");
+        monthMap.put(4, "April");
+        monthMap.put(5, "May");
+        monthMap.put(6, "June");
+        monthMap.put(7, "July");
+        monthMap.put(8, "August");
+        monthMap.put(9, "September");
+        monthMap.put(10, "October");
+        monthMap.put(11, "November");
+        monthMap.put(12, "December");
+    }
+
+
+
+}
