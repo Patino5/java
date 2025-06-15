a31cabffad1d4ec42f8407f5de1e174b41a448dd
Patino5 <ryan.patino5@gmail.com>
2025-06-15 01:05:57 -0500

    help method added and implemented in tryIncantation method when "help" entered

--- SpellBook-exercise/src/SpellBook.java
+++ SpellBook-exercise/src/SpellBook.java
@@ -14,12 +14,25 @@ public class SpellBook {
      }
 
      public void tryIncantation(String incantation) {
+          if (incantation.equalsIgnoreCase("help")){
+               help();
+               return;
+          }
           for (Spell spell : spells){
                if (spell.getIncantation().equalsIgnoreCase(incantation.trim())) {
                     spell.cast();
                     return;
                }
+
           }
           System.out.println("The spell fizzled out! Try again!");
+     }
+
+     public void help() {
+          System.out.println("\nIncantation\t| Description");
+          for (Spell spell : spells){
+               System.out.println(spell.getHelp());
+          }
+          System.out.println();
      }
 }
