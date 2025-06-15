8ba878930da8e30090b5fd352d3c1b97fe879979
Patino5 <ryan.patino5@gmail.com>
2025-06-14 23:52:20 -0500

    ExitSpell added

---
+++ SpellBook-exercise/src/ExitSpell.java
@@ -0,0 +1,10 @@
+public class ExitSpell implements Spell {
+    public void cast() {
+        System.out.println("A portal opens beneath your feet. Goodbye, wizard");
+        System.exit(0);
+    }
+
+    public String getIncantation() {
+        return "quit";
+    }
+}
