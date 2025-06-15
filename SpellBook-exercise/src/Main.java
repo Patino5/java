c7d8fbf2a7db6621b243e8dc4909b04d12e5ff66
Patino5 <ryan.patino5@gmail.com>
2025-06-15 00:25:29 -0500

    program running as expected, bonus challenge start

--- SpellBook-exercise/src/Main.java
+++ SpellBook-exercise/src/Main.java
@@ -1,12 +1,15 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         SpellBook spellBook = new SpellBook();
-        spellBook.tryIncantation("Ignis Orbis");
 
+        do {
+            System.out.print("Enter incantation to cast a spell: ");
+            String incantation = scanner.nextLine();
+            spellBook.tryIncantation(incantation);
+        } while (true);
 
     }
 }
