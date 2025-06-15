b98439b75771e5b56fe7c2c122fb2f14c072fd1e
Patino5 <ryan.patino5@gmail.com>
2025-06-14 23:51:53 -0500

    Time Stop spell added

---
+++ SpellBook-exercise/src/TimeStop.java
@@ -0,0 +1,9 @@
+public class TimeStop implements Spell {
+    public void cast() {
+        System.out.println("Time halts around you, the world frozen as you move freely through stillness.");
+    }
+
+    public String getIncantation() {
+        return "Tempora Fractum";
+    }
+}
