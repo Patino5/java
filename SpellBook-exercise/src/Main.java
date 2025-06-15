1c89c388e71b5b5a7d5188d46f86d0a16975f7e5
Patino5 <ryan.patino5@gmail.com>
2025-06-15 00:08:55 -0500

    Main added with Scanner and SpellBook initialized

---
+++ SpellBook-exercise/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        SpellBook spellBook = new SpellBook();
+        spellBook.tryIncantation("Ignis Orbis");
+
+
+    }
+}
