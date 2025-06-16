37aad55184e1a1409b9fd9e0143a46414fa6712c
Patino5 <ryan.patino5@gmail.com>
2025-06-16 16:21:49 -0500

    TemperatureConverter class and test class added

---
+++ exercise-stateless-unit-testing/src/test/java/org/example/TemperatureConverterTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class TemperatureConverterTest extends TestCase {
+
+    public void testCelsiusToFahrenheit() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+
+    public void testFahrenheitToCelsius() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+
+    public void testCelsiusToKelvin() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+
+    public void testKelvinToCelsius() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+
+    public void testFahrenheitToKelvin() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+
+    public void testKelvinToFahrenheit() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+}
