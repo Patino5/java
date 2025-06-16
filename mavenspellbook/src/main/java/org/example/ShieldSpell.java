daf03115b774e36b734f22026ab4cd2e22b86627
Patino5 <ryan.patino5@gmail.com>
2025-06-16 10:45:38 -0500

    Shield Spell added

---
+++ mavenspellbook/src/main/java/org/example/ShieldSpell.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ShieldSpell implements Spell {
+    public void cast() {
+        System.out.println("A translucent barrier forms around you, absorbing the incoming strike.");
+    }
+
+    public String getIncantation() {
+        return "Protect";
+    }
+}
