2e36ab8f70b038031c4547315f9aa2f5d89ca7f2
Patino5 <ryan.patino5@gmail.com>
2025-06-16 11:29:25 -0500

    added help() to display all incantation and short description of spells when help is entered

--- mavenspellbook/src/main/java/org/example/SpellBook.java
+++ mavenspellbook/src/main/java/org/example/SpellBook.java
@@ -16,6 +16,10 @@ public class SpellBook {
     }
 
     public void tryIncantation(String incantation){
+        if (incantation.equalsIgnoreCase("help")){
+            help();
+            return;
+        }
         for (Spell spell : spells) {
             if (spell.getIncantation().equalsIgnoreCase(incantation)){
                 spell.cast();
@@ -23,5 +27,14 @@ public class SpellBook {
             }
         }
         System.out.println("The spell fizzled out! Try again.");
+    }
+
+    public void help(){
+        System.out.println("--------------------------");
+        System.out.println("INCANTATION\t| DESCRIPTION");
+        System.out.println("--------------------------");
+        for (Spell spell : spells) {
+            System.out.println(spell.getHelp());
+        }
     }
 }
