3c9597d289f4fcded83deca7f0fadf540842a6bb
Patino5 <ryan.patino5@gmail.com>
2025-06-18 12:13:09 -0500

    switch statement for control flow added and display menu

--- summative-shopping-cart/src/main/java/inc/shopping/App.java
+++ summative-shopping-cart/src/main/java/inc/shopping/App.java
@@ -1,10 +1,46 @@
 package inc.shopping;
 
-/**
- * Hello world!
- */
+import inc.shopping.view.TerminalUtils;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        TerminalUtils io = new TerminalUtils();
+
+        // application loop
+        while (true) {
+            io.displayMessage("Main Menu");
+            io.displayMessage("1. Display Cart");
+            io.displayMessage("2. Remove an Item");
+            io.displayMessage("3. Add an Item");
+            io.displayMessage("4. Checkout");
+            io.displayMessage("5. Exit");
+
+            int choice = io.getIntRequired("Enter a menu choice by number");
+
+            switch (choice) {
+                case 1 -> {
+                    // Display cart
+                    io.displayMessage("Cart contents:");
+                }
+                case 2 -> {
+                    // Remove Item from cart
+                    io.displayMessage("Item/s available to remove:");
+                }
+                case 3 -> {
+                    // Add Item to cart
+                    io.displayMessage("Item to add:");
+                }
+                case 4 -> {
+                    // Checkout
+                    io.displayMessage("Checking out cart...");
+                }
+                case 5 -> {
+                    // exit
+                    io.displayMessage("Thank you for using the Shopping Cart App!");
+                    System.exit(0);
+                }
+            }
+
+        }
     }
 }
