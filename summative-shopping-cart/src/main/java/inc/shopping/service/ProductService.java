9119e093a30f7b1a581a217380cbe57c035b7bb7
Patino5 <ryan.patino5@gmail.com>
2025-06-18 15:29:56 -0500

    ProductService class added

---
+++ summative-shopping-cart/src/main/java/inc/shopping/service/ProductService.java
@@ -0,0 +1,33 @@
+package inc.shopping.service;
+
+import inc.shopping.model.Product;
+import inc.shopping.view.TerminalUtils;
+
+import java.util.ArrayList;
+
+public class ProductService {
+    TerminalUtils io = new TerminalUtils();
+    private static ArrayList<Product> cart = new ArrayList<>();
+
+    //what do we want our cart to do
+    // display item in cart
+    public void displayCart() {
+        if (cart.isEmpty()) {
+            io.displayMessage("Cart is currently empty.");
+            return;
+        }
+        io.displayMessage("PRODUCT |  PRICE  |  QTY  | SUBTOTAL");
+        for (Product product : cart) {
+            io.displayMessage(product.getDetails());
+        }
+
+    }
+    // add item to cart
+    public void addProduct(Product product) {
+        cart.add(product);
+    }
+    // remove item from cart
+
+    // check out
+
+}
