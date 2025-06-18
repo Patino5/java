08fb336de8227ae4ea36fa36f7052dbf1a0930ad
Patino5 <ryan.patino5@gmail.com>
2025-06-18 15:28:56 -0500

    Product class added

---
+++ summative-shopping-cart/src/main/java/inc/shopping/model/Product.java
@@ -0,0 +1,47 @@
+package inc.shopping.model;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity){
+        setName(name);
+        setPrice(price);
+        setQuantity(quantity);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private double getSubtotal() {
+        return this.quantity * this.price;
+    }
+
+    public String getDetails() {
+        return String.format(
+                "%-7.7s | $%6.2f | %5d | $%8.2f",
+                getName(), getPrice(), getQuantity(), getSubtotal());
+    }
+}
