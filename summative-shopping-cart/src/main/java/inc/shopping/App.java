d956d885f0a1ebe7a9d4ae3d08d1ff68fc9481d6
Patino5 <ryan.patino5@gmail.com>
2025-06-19 10:56:12 -0500

    added default to switch, changed to displayMainMenu() to display menu, added opening message

--- summative-shopping-cart/src/main/java/inc/shopping/App.java
+++ summative-shopping-cart/src/main/java/inc/shopping/App.java
@@ -1,35 +1,23 @@
 package inc.shopping;
 
 import inc.shopping.model.Product;
-import inc.shopping.service.ProductService;
+import inc.shopping.service.CartService;
 import inc.shopping.view.TerminalUtils;
 
 public class App {
     public static void main(String[] args) {
         TerminalUtils io = new TerminalUtils();
-        ProductService cart = new ProductService();
+        CartService cart = new CartService();
 
+        io.displayMessage("Shopping Inc. Shopping Cart System");
         // application loop
         while (true) {
-            io.displayMessage("Main Menu");
-            io.displayMessage("1. Display Cart");
-            io.displayMessage("2. Remove an Item");
-            io.displayMessage("3. Add an Item");
-            io.displayMessage("4. Checkout");
-            io.displayMessage("5. Exit");
-
+            io.displayMainMenu();
             int choice = io.getIntRequired("Enter a menu choice by number");
 
             switch (choice) {
-                case 1 -> {
-                    // Display cart
-                    io.displayMessage("Cart contents:");
-                    cart.displayCart();
-                }
-                case 2 -> {
-                    // Remove Item from cart
-                    io.displayMessage("Item/s available to remove:");
-                }
+                case 1 -> cart.displayCart();
+                case 2 -> io.displayMessage("Item/s available to remove:"); // Remove Item from cart
                 case 3 -> {
                     // Add Item to cart
                     Product newProduct =
@@ -50,6 +38,7 @@ public class App {
                     io.displayMessage("Thank you for using the Shopping Cart App!");
                     System.exit(0);
                 }
+                default -> io.displayMessage("Invalid choice. Please select again");
             }
 
         }
