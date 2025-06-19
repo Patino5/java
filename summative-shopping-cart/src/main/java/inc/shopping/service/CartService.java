7ac0afbc030bbe840affc6609d0b7940072a81df
Patino5 <ryan.patino5@gmail.com>
2025-06-19 12:12:59 -0500

    changed cart to use hashmap vs arraylist, updated addProduct - will need to update for duplicate items to add to quantity vs overide qty

--- summative-shopping-cart/src/main/java/inc/shopping/service/CartService.java
+++ summative-shopping-cart/src/main/java/inc/shopping/service/CartService.java
@@ -4,10 +4,11 @@ import inc.shopping.model.Product;
 import inc.shopping.view.TerminalUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CartService {
     TerminalUtils io = new TerminalUtils();
-    private static ArrayList<Product> cart = new ArrayList<>();
+    private HashMap<String, Product > cart = new HashMap<>();
 
     //what do we want our cart to do
     // display item in cart
@@ -17,16 +18,19 @@ public class CartService {
             return;
         }
         io.displayMessage("PRODUCT |  PRICE  |  QTY  | SUBTOTAL");
-        for (Product product : cart) {
+        for (Product product : cart.values()) {
             io.displayMessage(product.getDetails());
         }
+        io.getString("Press enter to continue.");
+    }
 
-    }
     // add item to cart
-    public void addProduct(Product product) {
-        cart.add(product);
+    public void addProduct(String productName, double price, int quantity) {
+        cart.put(productName, new Product(productName, price, quantity));
     }
+
     // remove item from cart
+
 
     // check out
 
