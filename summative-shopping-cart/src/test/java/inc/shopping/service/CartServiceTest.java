4861b415ffa95121d3646a41c8147c1777e6d875
Patino5 <ryan.patino5@gmail.com>
2025-06-19 14:53:21 -0500

    added service test file

---
+++ summative-shopping-cart/src/test/java/inc/shopping/service/CartServiceTest.java
@@ -0,0 +1,20 @@
+package inc.shopping.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartServiceTest {
+
+    @Test
+    void displayCart() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void checkout() {
+    }
+}
