b263d9a770d6e36394b1385e2628ca68047d95c3
Patino5 <ryan.patino5@gmail.com>
2025-06-19 17:12:18 -0500

    changed addProduct to select from an ArrayList of initialized products, displayProducts() displays product available, addProductToCart() checks for item in cart and add it or updates quantity if already in cart

--- summative-shopping-cart/src/main/java/inc/shopping/service/CartService.java
+++ summative-shopping-cart/src/main/java/inc/shopping/service/CartService.java
@@ -3,14 +3,32 @@ package inc.shopping.service;
 import inc.shopping.model.Product;
 import inc.shopping.view.TerminalUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CartService {
-    TerminalUtils io = new TerminalUtils();
-    private HashMap<String, Product > cart = new HashMap<>();
+    private TerminalUtils io;
+    private HashMap<String, Product > cart;
+    private ArrayList<Product> productList;
 
-    //what do we want our cart to do
-    // display item in cart
+    public CartService(TerminalUtils io) {
+        this.io = io;
+        this.cart = new HashMap<>();
+        this.productList = new ArrayList<>();
+        initialProducts();
+    }
+
+    public void initialProducts() {
+        productList.add(new Product("Baseball", 5.99, 0));
+        productList.add(new Product("Basketball", 34.99, 0));
+        productList.add(new Product("Soccerball", 29.00, 0));
+        productList.add(new Product("Football", 24.99, 0));
+        productList.add(new Product("Desk", 350, 0));
+        productList.add(new Product("Chair", 265, 0));
+        productList.add(new Product("Headset", 359, 0));
+    }
+
+    // display item/s in cart
     public void displayCart() {
         if (cart.isEmpty()) {
             io.displayMessage("Cart is currently empty.");
@@ -24,15 +42,27 @@ public class CartService {
     }
 
     // add item to cart
-    public void addProduct(String productName, double price, int quantity) {
-        cart.put(productName, new Product(productName, price, quantity));
-        io.displayMessage(quantity + " " + productName + (quantity > 1 ? "s added to cart." : " added to cart."));
+    public void addProduct() {
+        displayProducts();
+        int choice = io.getIntRequired("Select a product by number") - 1;
+
+        if (choice < 0 || choice >= productList.size()) {
+            io.displayMessage("not a valid product. choose between 1 and " + productList.size());
+            return;
+        }
+
+        Product selectedProduct = productList.get(choice);
+        int quantity = io.getIntRequired("Enter quantity to add to cart");
+
+        addProductToCart(selectedProduct.getName(), selectedProduct.getPrice(), quantity);
+        io.displayMessage(quantity + " " + selectedProduct.getName() +
+                (quantity > 1 ? "s added to cart." : " added to cart."));
     }
 
     // remove item from cart
 
 
-    // check out
+    // Cart check out
     public void checkout() {
         double total = 0;
         for (Product product : cart.values()) {
@@ -40,6 +70,22 @@ public class CartService {
         }
         cart.clear();
         io.displayMessage("Total Due: $" + total);
+    }
+
+    private void displayProducts(){
+        io.displayMessage("Available Products");
+        for (int i = 0; i < productList.size(); i++) {
+            io.displayMessage(String.format((i + 1) + ". " +
+                    "%-10s | $%6.2f", productList.get(i).getName(), productList.get(i).getPrice()));
+        }
+    }
+
+    private void addProductToCart(String productName, double price, int quantity) {
+        if (cart.containsKey(productName)) {
+            cart.get(productName).addQuantity(quantity);
+        } else {
+            cart.put(productName, new Product(productName, price, quantity));
+        }
     }
 
 }
