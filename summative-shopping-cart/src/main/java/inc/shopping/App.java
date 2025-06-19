30ad99f0ddfd5ed8ff95af990529807f24059e13
Patino5 <ryan.patino5@gmail.com>
2025-06-19 17:07:04 -0500

    exitMessage() added to clean up switch statement

--- summative-shopping-cart/src/main/java/inc/shopping/App.java
+++ summative-shopping-cart/src/main/java/inc/shopping/App.java
@@ -6,7 +6,7 @@ import inc.shopping.view.TerminalUtils;
 public class App {
     public static void main(String[] args) {
         TerminalUtils io = new TerminalUtils();
-        CartService cart = new CartService();
+        CartService cart = new CartService(io);
 
         io.displayMessage("Shopping Inc. Shopping Cart System");
         // application loop
@@ -17,19 +17,15 @@ public class App {
             switch (choice) {
                 case 1 -> cart.displayCart();
                 case 2 -> io.displayMessage("Item/s available to remove:"); // Remove Item from cart
-                case 3 -> cart.addProduct(
-                            io.getStringRequired("Enter name of product"),
-                            io.getDouble("Enter Price of product"),
-                            io.getIntRequired("Enter quantity to add")
-                            );
+                case 3 -> cart.addProduct();
                 case 4 -> cart.checkout();
-                case 5 -> {
-                    io.displayMessage("Thank you for using the Shopping Cart App!");
-                    System.exit(0);
-                }
+                case 5 -> exitMessage(io);
                 default -> io.displayMessage("Invalid choice. Please select again");
             }
-
         }
+    }
+    private static void exitMessage(TerminalUtils io){
+        io.displayMessage("Thank you for using the Shopping Cart App!");
+        System.exit(0);
     }
 }
