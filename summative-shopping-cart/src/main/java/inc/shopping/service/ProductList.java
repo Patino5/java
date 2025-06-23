b590c72f4604e1077da136dba55fc992d17680c2
Patino5 <ryan.patino5@gmail.com>
2025-06-23 17:13:52 -0500

    added ProductList that uses an arraylist to have items that can be selected and added to list

---
+++ summative-shopping-cart/src/main/java/inc/shopping/service/ProductList.java
@@ -0,0 +1,44 @@
+package inc.shopping.service;
+
+import inc.shopping.model.Product;
+
+import java.util.ArrayList;
+
+public class ProductList {
+    private final ArrayList<Product> availableProducts;
+
+    public ProductList() {
+        this.availableProducts = new ArrayList<>();
+        initializeAvailableProducts();
+    }
+
+    private void initializeAvailableProducts() {
+        availableProducts.add(new Product("Apple", .99, 0));
+        availableProducts.add(new Product("Orange", .89, 0));
+        availableProducts.add(new Product("Banana", .79, 0));
+        availableProducts.add(new Product("Milk", 4.89, 0));
+        availableProducts.add(new Product("Cheese", 4.50, 0));
+        availableProducts.add(new Product("Ham", 7.99, 0));
+        availableProducts.add(new Product("Butter", 5.99, 0));
+        availableProducts.add(new Product("Eggs", 9.99, 0));
+    }
+
+    public ArrayList<Product> getAvailableProducts() {
+        return  new ArrayList<>(availableProducts);
+    }
+
+    public Product getProductByIndex(int index) {
+        if (index < 0 || index >= availableProducts.size()) {
+            return null;
+        }
+        return availableProducts.get(index);
+    }
+
+    public int getListSize() {
+        return availableProducts.size();
+    }
+
+    public boolean isEmpty() {
+        return availableProducts.isEmpty();
+    }
+}
