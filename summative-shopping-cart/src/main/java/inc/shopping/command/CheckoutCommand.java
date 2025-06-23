230fe54131491f3883d027c20d433734d8e0e14f
Patino5 <ryan.patino5@gmail.com>
2025-06-23 11:03:41 -0500

    added formatting to display items scanned

--- summative-shopping-cart/src/main/java/inc/shopping/command/CheckoutCommand.java
+++ summative-shopping-cart/src/main/java/inc/shopping/command/CheckoutCommand.java
@@ -1,5 +1,6 @@
 package inc.shopping.command;
 
+import inc.shopping.model.Product;
 import inc.shopping.service.ShoppingCartService;
 import inc.shopping.view.TerminalUtils;
 
@@ -13,8 +14,27 @@ public class CheckoutCommand implements Command {
     }
 
     public void execute() {
-        double total = basket.checkout();
-        io.displayMessage(String.format("Total: $%.2f", total));
-        io.displayMessage("Cart is empty. Thank you");
+        int itemTotal = 0;
+        Product productDetails;
+
+        if(!basket.getCart().isEmpty()){
+            io.displayMessage("\nScanning items in cart...");
+
+            for (String key : basket.getCart().keySet()){
+                productDetails = basket.getCart().get(key);
+                io.displayMessage( productDetails.getQuantity() +
+                        " " + productDetails.getName() +
+                        (productDetails.getQuantity() > 1 ? "s " : " ") + "scanned. ");
+
+                itemTotal += basket.getCart().get(key).getQuantity();
+            }
+
+            double total = basket.checkout();
+            io.displayMessage("Total Items: "+ itemTotal);
+            io.displayMessage(String.format("Total: $%.2f", total));
+
+        } else io.displayMessage("Cart is empty. Thank you");
+        io.getString("\nPress enter to continue");
+
     }
 }
