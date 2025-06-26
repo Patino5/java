08455ca0a519930ecb28acc865914f84f8128bfb
Patino5 <ryan.patino5@gmail.com>
2025-06-26 16:38:48 -0500

    ui package with TextIO interface added

---
+++ summative-library-management-system/src/main/java/learn/ui/TextIO.java
@@ -0,0 +1,17 @@
+package learn.ui;
+
+public interface TextIO {
+    void prinln(Object value);
+
+    void print(Object value);
+
+    void printf(String format, Object... values);
+
+    String readString(String prompt);
+
+    boolean readBoolean(String prompt);
+
+    int readInt(String prompt);
+
+    int readInt(String prompt, int min, int max);
+}
