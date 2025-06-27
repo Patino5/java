9c030b711e048b276f854a93591edfe75cef6bbe
Patino5 <ryan.patino5@gmail.com>
2025-06-27 14:34:06 -0500

    added HashMap and details it will hold

--- airport-terminal-management-system/src/main/com/airport/domain/reservation/ReservationSystem.java
+++ airport-terminal-management-system/src/main/com/airport/domain/reservation/ReservationSystem.java
@@ -1,4 +1,15 @@
 package airport.domain.reservation;
 
+import airport.domain.model.Passenger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ReservationSystem {
+    // key - flight number , value - list of passengers
+    private HashMap<String, ArrayList<Passenger>> reservations;
+
+    // Add passenger to a flight
+
+    // Retrieve all passengers booked on a specific flight
 }
