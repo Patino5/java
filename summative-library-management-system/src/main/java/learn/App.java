b3c5d62686504d0a714a58b0b264b8f89a64e190
Patino5 <ryan.patino5@gmail.com>
2025-06-27 12:39:08 -0500

    App uses BookFileRepository with path to books.txt and calls the controller to run program

--- summative-library-management-system/src/main/java/learn/App.java
+++ summative-library-management-system/src/main/java/learn/App.java
@@ -1,10 +1,22 @@
 package learn;
 
-/**
- * Hello world!
- */
+import learn.data.BookFileRepository;
+import learn.domain.BookService;
+import learn.ui.ConsoleIO;
+import learn.ui.Controller;
+import learn.ui.View;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // "./data/books.txt" is the path to "production" data file.
+        BookFileRepository repository = new BookFileRepository("./data/books.txt");
+        BookService service = new BookService(repository);
+
+        ConsoleIO io = new ConsoleIO();
+        View view = new View(io);
+
+        Controller controller = new Controller(view, service);
+
+        controller.run();
     }
 }
