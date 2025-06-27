19c38d4e2fd9935b12e5f54c562e3d3b65544bfe
Patino5 <ryan.patino5@gmail.com>
2025-06-27 17:06:50 -0500

    added addtional attributes to constructor

--- airport-terminal-management-system/src/main/com/airport/domain/model/PrivateJet.java
+++ airport-terminal-management-system/src/main/com/airport/domain/model/PrivateJet.java
@@ -4,7 +4,9 @@ public class PrivateJet extends Aircraft {
     private boolean hasLuxuryService; // true if lux service available
     private int maxSpeed; // jet speed in km/h
 
-    public PrivateJet(String model, int capacity, double fuelCapacity) {
+    public PrivateJet(String model, int capacity, double fuelCapacity,boolean hasLuxuryService, int maxSpeed) {
         super(model, capacity, fuelCapacity);
+        this.hasLuxuryService = hasLuxuryService;
+        this.maxSpeed = maxSpeed;
     }
 }
