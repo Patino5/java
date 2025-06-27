89b573aafd2082a9cb2e465701124ada718d0f7d
Patino5 <ryan.patino5@gmail.com>
2025-06-27 16:58:17 -0500

    added DataAccessException

---
+++ airport-terminal-management-system/src/main/com/airport/data/DataAccessException.java
@@ -0,0 +1,12 @@
+package airport.data;
+
+public class DataAccessException extends Exception {
+
+    public DataAccessException(String message) {
+        super(message);
+    }
+
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
