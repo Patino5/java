5717428c26800cf3083bb56de834c8332317a886
Patino5 <ryan.patino5@gmail.com>
2025-07-03 14:37:20 -0500

    ReservaionSystem added a hashmap for flight tracking as well

--- airport-terminal-management/src/main/java/airport/domain/reservation/ReservationSystem.java
+++ airport-terminal-management/src/main/java/airport/domain/reservation/ReservationSystem.java
@@ -1,12 +1,35 @@
 package airport.domain.reservation;
 
+import airport.domain.model.Flight;
 import airport.domain.model.Passenger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ReservationSystem {
-    private HashMap<String, ArrayList<Passenger>> reservations;
+    private final HashMap<String, ArrayList<Passenger>> reservations;
+    private final HashMap<String, Flight> flights;
+
+    public ReservationSystem() {
+        this.reservations = new HashMap<>();
+        this.flights = new HashMap<>();
+    }
+
+    public HashMap<String, ArrayList<Passenger>> getReservations() {
+        return new HashMap<>(reservations);
+    }
+
+    public void initializeFlights(ArrayList<Flight> flights) {
+        for (Flight flight : flights) {
+            this.flights.put(flight.getFlightNumber(), flight);
+            this.reservations.putIfAbsent(flight.getFlightNumber(), new ArrayList<>());
+        }
+    }
+
+    public void addFlight(Flight flight) {
+        flights.put(flight.getFlightNumber(), flight);
+        reservations.putIfAbsent(flight.getFlightNumber(), new ArrayList<>());
+    }
 
     public void addReservation(String flightNumber, Passenger passenger) {
         if (reservations.containsKey(flightNumber)) {
@@ -22,7 +45,16 @@ public class ReservationSystem {
         }
     }
 
+    public void addReservation(String flightNumber) {
+        ArrayList<Passenger> emptyPassengerList = new ArrayList<>();
+        reservations.put(flightNumber, emptyPassengerList);
+    }
+
     public ArrayList<Passenger> getPassengersForFlight(String flightNumber) {
         return reservations.get(flightNumber);
+    }
+
+    public Flight getFlight(String flightNumber) {
+        return flights.get(flightNumber);
     }
 }
