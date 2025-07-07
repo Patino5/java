009f658911f648810c7182fec8d896941cbeaa1e
Patino5 <ryan.patino5@gmail.com>
2025-07-07 16:38:31 -0500

    InventoryManagerApplication setup

--- inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
+++ inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
@@ -1,13 +1,40 @@
 package com.learn.inventory_manager;
 
+import com.learn.inventory_manager.service.InventoryService;
+import com.learn.inventory_manager.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.List;
 
 @SpringBootApplication
-public class InventoryManagerApplication {
+public class InventoryManagerApplication implements CommandLineRunner {
+	@Autowired
+	private InventoryService inventoryService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("=== Inventory Manager ===");
+
+		List<Product> productList = inventoryService.getAvailableInventory();
+		displayProducts(productList);
+
+	}
+
+	private static void displayProducts(List<Product> productList) {
+		System.out.println("Total Products: " + productList.size());
+		System.out.println();
+
+		for (Product product : productList) {
+			System.out.println(product.toString());
+			System.out.println();
+		}
 	}
 
 }
