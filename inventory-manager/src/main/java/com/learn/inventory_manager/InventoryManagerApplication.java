ec2781de976ec3d73359748308e0bc9006328887
Patino5 <ryan.patino5@gmail.com>
2025-07-08 16:01:07 -0500

    testing service and file writing in App.

--- inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
+++ inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
@@ -1,5 +1,6 @@
 package com.learn.inventory_manager;
 
+import com.learn.inventory_manager.repository.DataAccessException;
 import com.learn.inventory_manager.service.InventoryService;
 import com.learn.inventory_manager.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @SpringBootApplication
@@ -19,11 +21,28 @@ public class InventoryManagerApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) throws DataAccessException {
 		System.out.println("=== Inventory Manager ===");
 
 		List<Product> productList = inventoryService.getAvailableInventory();
 		displayProducts(productList);
+		System.out.println(inventoryService.getProductById("2").toString());
+		System.out.println(inventoryService.getProductByName("keyboard").toString());
+		System.out.println(inventoryService.deleteById("2").isSuccess());
+		System.out.println(inventoryService.deleteById("2").isSuccess());
+		System.out.println(inventoryService.deleteById("2").getErrorMessages());
+
+		productList = inventoryService.getAvailableInventory();
+		displayProducts(productList);
+
+		Product newProduct = new Product("2", "Phone", 5, new BigDecimal("1249.99"));
+		inventoryService.add(newProduct);
+		newProduct.setQuantity(newProduct.getQuantity() + 19);
+		inventoryService.update(newProduct);
+		productList = inventoryService.getAvailableInventory();
+
+		displayProducts(productList);
+
 
 	}
 
