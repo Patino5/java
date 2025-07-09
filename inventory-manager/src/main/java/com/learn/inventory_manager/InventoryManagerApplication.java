c8d09330dca4e788a3f5072de4e2b84f099a9709
Patino5 <ryan.patino5@gmail.com>
2025-07-09 14:58:19 -0500

    cleanup of namespace and data file

--- inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
+++ inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
@@ -2,9 +2,7 @@ package com.learn.inventory_manager;
 
 import com.learn.inventory_manager.repository.DataAccessException;
 import com.learn.inventory_manager.service.InventoryService;
-import com.learn.inventory_manager.ui.ConsoleIO;
 import com.learn.inventory_manager.ui.Controller;
-import com.learn.inventory_manager.ui.View;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
