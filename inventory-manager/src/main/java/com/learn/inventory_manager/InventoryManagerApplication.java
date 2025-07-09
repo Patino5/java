435581b21ea93c735dcddf996f83ff4bf8dc545e
Patino5 <ryan.patino5@gmail.com>
2025-07-09 14:57:28 -0500

    Autowired controller and inventoryService

--- inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
+++ inventory-manager/src/main/java/com/learn/inventory_manager/InventoryManagerApplication.java
@@ -2,19 +2,23 @@ package com.learn.inventory_manager;
 
 import com.learn.inventory_manager.repository.DataAccessException;
 import com.learn.inventory_manager.service.InventoryService;
-import com.learn.inventory_manager.model.Product;
+import com.learn.inventory_manager.ui.ConsoleIO;
+import com.learn.inventory_manager.ui.Controller;
+import com.learn.inventory_manager.ui.View;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
-import java.math.BigDecimal;
-import java.util.List;
 
 @SpringBootApplication
 public class InventoryManagerApplication implements CommandLineRunner {
+
 	@Autowired
 	private InventoryService inventoryService;
+
+	@Autowired
+	private Controller controller;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagerApplication.class, args);
@@ -22,38 +26,6 @@ public class InventoryManagerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws DataAccessException {
-		System.out.println("=== Inventory Manager ===");
-
-		List<Product> productList = inventoryService.getAvailableInventory();
-		displayProducts(productList);
-		System.out.println(inventoryService.getProductById("2").toString());
-		System.out.println(inventoryService.getProductByName("keyboard").toString());
-		System.out.println(inventoryService.deleteById("2").isSuccess());
-		System.out.println(inventoryService.deleteById("2").isSuccess());
-		System.out.println(inventoryService.deleteById("2").getErrorMessages());
-
-		productList = inventoryService.getAvailableInventory();
-		displayProducts(productList);
-
-		Product newProduct = new Product("2", "Phone", 5, new BigDecimal("1249.99"));
-		inventoryService.add(newProduct);
-		newProduct.setQuantity(newProduct.getQuantity() + 19);
-		inventoryService.update(newProduct);
-		productList = inventoryService.getAvailableInventory();
-
-		displayProducts(productList);
-
-
+		controller.run();
 	}
-
-	private static void displayProducts(List<Product> productList) {
-		System.out.println("Total Products: " + productList.size());
-		System.out.println();
-
-		for (Product product : productList) {
-			System.out.println(product.toString());
-			System.out.println();
-		}
-	}
-
 }
